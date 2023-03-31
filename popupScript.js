1a1c7acd333af7ced3cff48733340c24dbd1d153
dorian3343 <129544899+dorian3343@users.noreply.github.com>
2023-03-31 23:44:02 +0200

    Saving choice js

--- popupScript.js
+++ popupScript.js
@@ -1,8 +1,20 @@
-const enlarge = document.getElementById('enlarge')
-const enlargeRange = document.getElementById('enlargeRange')
-const rangeDiv = document.getElementById('rangeDiv')
-const rangeResult = document.getElementById('rangeResult')
-
+const enlarge = document.getElementById('enlarge');
+const enlargeRange = document.getElementById('enlargeRange');
+const rangeDiv = document.getElementById('rangeDiv');
+const rangeResult = document.getElementById('rangeResult');
+var size = "";
+function check(){
+    let size = localStorage.getItem("fontsize");
+            console.log("Checkform is running. . .")
+            if (size == "")
+            {
+                formsend();
+            }
+            else
+            {
+                console.log("The size is: " + size);
+            }
+        }
 enlarge.addEventListener('change', function() {
     if (enlarge.checked) {
         rangeDiv.classList.remove('hide');
@@ -10,10 +22,16 @@ enlarge.addEventListener('change', function() {
         rangeDiv.classList.add('hide');
     }
 })
-
 enlargeRange.addEventListener('change', () => {
-    var value = enlargeRange.value
-    
-    rangeResult.innerHTML = value + " px"
+  console.log("formsend is running. . .");
+    localStorage.setItem("fontsize", enlargeRange.value);
+    let size = localStorage.getItem("fontsize");
+    rangeResult.innerHTML = size + " px"
+        if (size == "")
+            {
+                console.log("Empty!");
+            }
+            else {
+                console.log("The size is: " + size);
+            }
 })
-
