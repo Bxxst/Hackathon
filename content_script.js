d7e24b83d7591bcdc98d502293c6acc6503fb4c3
Bxxst_ <83009888+Bxxst@users.noreply.github.com>
2023-04-01 03:33:12 +0200

    logo and buttons onhover

--- content_script.js
+++ content_script.js
@@ -1,3 +1,9 @@
 document.querySelectorAll("a").forEach(function(el) {
   el.setAttribute("data-text", el.innerText);
 });
+document.querySelectorAll("button").forEach(function(el) {
+    el.setAttribute("data-text", el.innerText);
+});
+document.querySelectorAll("input[type=submit]").forEach(function(el) {
+    el.setAttribute("data-text", el.innerText);
+});
