222b7375c1964273469eefca8474c2ff5957828e
dorian3343 <129544899+dorian3343@users.noreply.github.com>
2023-04-01 03:46:18 +0200

    Script saving between websites

--- popupScript.js
+++ popupScript.js
@@ -1,64 +1,83 @@
-const enlarge = document.getElementById('pill')
-const enlargeRange = document.getElementById('enlargeRange')
-const rangeDiv = document.getElementById('rangeDiv')
-const rangeResult = document.getElementById('rangeResult')
-const btn = document.getElementById('btn')
-
+const enlarge = document.getElementById('enlarge');
+const enlargeRange = document.getElementById('enlargeRange');
+const rangeDiv = document.getElementById('rangeDiv');
+const rangeResult = document.getElementById('rangeResult');
 //getting the values of the checkboxes
 var size = "";
-var sizetoggle = 0;
-//0= off, 1= on
-
+document.addEventListener("DOMContentLoaded", function(){ check(); }, false);
 function check(){
+    chrome.storage.local.get(["key"]).then((result) => {
+  console.log("Toggle currently is " + result.key);
+  console.log(result.key);
+    if (result.key === true) {
+            document.getElementById("enlarge").checked = true;
+            rangeDiv.classList.remove('hide');
 
-    let size = localStorage.getItem("fontsize");
-    let toggletest = localStorage.getItem("togglesizestorage");
-
-    if (toggletest == "true") {
-                document.getElementById("enlarge").checked = true;
-                rangeDiv.classList.remove('hide');
-                enlargeRange.value = size;
     }
-     rangeResult.innerHTML = size + " px"
+
+});
+        chrome.storage.local.get(["key2"]).then((result) => {
+  console.log("Value currently is " + result.key2);
+       rangeResult.innerHTML = result.key2 + " px"
+       document.getElementById("enlargeRange").defaultValue = result.key2; 
+});
+  
+ chrome.storage.local.get(["key3"]).then((result) => {
+  console.log("Toggle2 currently is " + result.key3);
+    if (result.key3 === true) {
+            document.getElementById("window2").checked = true;
+
+    }
+    });
 }
-// checking the status of the inptuts from the website
+//checking the status of the inptuts from the website
+
 enlarge.addEventListener('change', function() {
-        localStorage.setItem("togglesizestorage", enlarge.checked);
-        let toggletest = localStorage.getItem("togglesizestorage");
+var toggletest = enlarge.checked;
+chrome.storage.local.set({ key: toggletest }).then(() => {
+  console.log("Toggle is set to " + toggletest);
+});
+
+chrome.storage.local.get(["key"]).then((result) => {
+  console.log("Toggle currently is " + result.key);
+});
+
     if (enlarge.checked) {
         rangeDiv.classList.remove('hide');
-        rangeDiv.classList.add('animation')
     } else {
         rangeDiv.classList.add('hide');
-        rangeDiv.classList.remove('animation')
     }
     if (toggletest == "true") {
-        document.getElementById("enlarge").checked = true;
+document.getElementById("enlarge").checked = true;
     }
 })
 //Checking changes for the toggle
 
 enlargeRange.addEventListener('change', () => {
-  console.log("formsend is running. . .");
-    localStorage.setItem("fontsize", enlargeRange.value);
-    let size = localStorage.getItem("fontsize");
+    size = enlargeRange.value;
+    chrome.storage.local.set({ key2: size }).then(() => {
+  console.log("Value is set to " + size);
+});
+    chrome.storage.local.get(["key2"]).then((result) => {
+  console.log("Value currently is " + result.key2);
+});
     rangeResult.innerHTML = size + " px"
         if (size == "")
             {
                 console.log("Empty!");
             }
-            else {
-                console.log("The size is: " + size);
-            }
-})
+    })
+
+window2.addEventListener('change', () => {
+    windowcheck = document.getElementById('window2').checked;
+    console.log(windowcheck);
+    chrome.storage.local.set({ key3: windowcheck }).then(() => {
+  console.log("Value is set to " + windowcheck);
+});
+    chrome.storage.local.get(["key3"]).then((result) => {
+  console.log("Value currently is " + result.key3);
+});
+    })
+
+
 //checking changes in the slider
-
-// btn.addEventListener('click', () => {
-//     let enlarge = document.getElementById('enlarge').checked
-
-//     if(enlarge){
-//         rangeDiv.classList.remove('hide');
-//     }else{
-//         rangeDiv.classList.add('hide')
-//     }
-// })
