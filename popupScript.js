0c18c1e5a93d12ce1d8e2413e6e3ebe3b6df6a5d
Jakub Piastowski <piastowskijakub@gmail.com>
2023-04-01 02:56:11 +0200

    Toggle checkboxe

--- popupScript.js
+++ popupScript.js
@@ -1,7 +1,9 @@
-const enlarge = document.getElementById('enlarge');
-const enlargeRange = document.getElementById('enlargeRange');
-const rangeDiv = document.getElementById('rangeDiv');
-const rangeResult = document.getElementById('rangeResult');
+const enlarge = document.getElementById('pill')
+const enlargeRange = document.getElementById('enlargeRange')
+const rangeDiv = document.getElementById('rangeDiv')
+const rangeResult = document.getElementById('rangeResult')
+const btn = document.getElementById('btn')
+
 //getting the values of the checkboxes
 var size = "";
 var sizetoggle = 0;
@@ -19,17 +21,19 @@ function check(){
     }
      rangeResult.innerHTML = size + " px"
 }
-//checking the status of the inptuts from the website
+// checking the status of the inptuts from the website
 enlarge.addEventListener('change', function() {
         localStorage.setItem("togglesizestorage", enlarge.checked);
         let toggletest = localStorage.getItem("togglesizestorage");
     if (enlarge.checked) {
         rangeDiv.classList.remove('hide');
+        rangeDiv.classList.add('animation')
     } else {
         rangeDiv.classList.add('hide');
+        rangeDiv.classList.remove('animation')
     }
     if (toggletest == "true") {
-document.getElementById("enlarge").checked = true;
+        document.getElementById("enlarge").checked = true;
     }
 })
 //Checking changes for the toggle
@@ -48,3 +52,13 @@ enlargeRange.addEventListener('change', () => {
             }
 })
 //checking changes in the slider
+
+// btn.addEventListener('click', () => {
+//     let enlarge = document.getElementById('enlarge').checked
+
+//     if(enlarge){
+//         rangeDiv.classList.remove('hide');
+//     }else{
+//         rangeDiv.classList.add('hide')
+//     }
+// })
